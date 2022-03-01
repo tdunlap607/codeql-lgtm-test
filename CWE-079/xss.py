aa14dc461317430afa45f8c3120173a2eec2601b
tdunlap607 <tdunlap607@gmail.com>
2022-03-01 15:29:44 -0500

    Resolve XSS in xx.py CWE-079

--- CWE-079/xss.py
+++ CWE-079/xss.py
@@ -10,4 +10,4 @@ def safe():
 @app.route('/unsafe')
 def unsafe_RENAME():
     first_name = request.args.get('name', '')
-    return make_response("Your name is " + first_name)
+    return make_response("Your name is " + escape(first_name))
