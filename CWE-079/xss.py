ccd247afb7af29403c9ebbcc2a77f3a009efd697
tdunlap607 <tdunlap607@gmail.com>
2022-03-01 14:43:27 -0500

    Rename and move CWE-079 xss.py vulnerable function. unsafe -> unsafe_RENAME

--- CWE-079/xss.py
+++ CWE-079/xss.py
@@ -2,12 +2,12 @@ from flask import Flask, request, make_response, escape
 
 app = Flask(__name__)
 
-@app.route('/unsafe')
-def unsafe():
-    first_name = request.args.get('name', '')
-    return make_response("Your name is " + first_name)
-
 @app.route('/safe')
 def safe():
     first_name = request.args.get('name', '')
     return make_response("Your name is " + escape(first_name))
+    
+@app.route('/unsafe')
+def unsafe_RENAME():
+    first_name = request.args.get('name', '')
+    return make_response("Your name is " + first_name)
