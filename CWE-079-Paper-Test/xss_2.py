799b8294bf233e4707de6d0acb06b4b058667807
tdunlap607 <tdunlap607@gmail.com>
2022-04-21 14:46:20 -0400

    two unsafe functions, same file

--- CWE-079-Paper-Test/xss_2.py
+++ CWE-079-Paper-Test/xss_2.py
@@ -2,12 +2,17 @@ from flask import Flask, request, make_response, escape
 
 app = Flask(__name__)
 
+@app.route('/unsafe_one')
+def unsafe_one():
+    first_name = request.args.get('name', '')
+    return make_response("Your name is " + first_name)
+    
 @app.route('/safe')
 def safe():
     first_name = request.args.get('name', '')
     return make_response("Your name is " + escape(first_name))
     
-@app.route('/unsafe')
-def unsafe():
+@app.route('/unsafe_two')
+def unsafe_two():
     first_name = request.args.get('name', '')
     return make_response("Your name is " + first_name)
