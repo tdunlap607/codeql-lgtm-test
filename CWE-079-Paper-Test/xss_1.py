aeee825d2742098eb89a3f4ccad7648f37ef05a1
tdunlap607 <tdunlap607@gmail.com>
2022-04-24 11:36:52 -0400

    Some refactors

--- CWE-079-Paper-Test/xss_1.py
+++ CWE-079-Paper-Test/xss_1.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 def safe():
     first_name = request.args.get('name', '')
     return make_response("Your name is " + escape(first_name))
-        
+    
+def new_function():
+    # the purpose of new function is to add new tex
+    print("A new function to shift unsafe() down")
+    print("More code...")
+    print("More code...")
+    print("More code...")
+    print("More code...")       
+ 
 @app.route('/unsafe')
 def unsafe():
     first_name = request.args.get('name', '')
